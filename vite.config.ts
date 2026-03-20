086912ef71e83e7e70431c61a85dca7c5b6ee63b
Nghia-93-996 <cow.coinofworld.vn@gmail.com>
2026-03-21 01:16:11 +0700

    fix(cors): cors coinofworld

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/cow-price': {
+        target: 'https://coinofworld.com',
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api\/cow-price/, '/api/price?time=30d&pair=COW%2FUSD'),
+      },
+    },
+  },
 })
